1d2c2f2802784b2b9afb82c09533e8f19f4b0857
techWith-Laxmi <lojha079@gmail.com>
2024-10-22 23:32:25 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,29 @@
+let input = document.querySelector("input");
+let btns = document.querySelectorAll("button");
+
+let string = "";
+let arr = Array.from(btns);
+
+arr.forEach(btn=>{
+    btn.addEventListener("click",(e)=>{
+        if(e.target.innerHTML == "AC")
+        {
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == '=')
+        {
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML === "DEL")
+        {
+            string = string.substring(0,string.length-1);
+            input.value = string;
+        }
+        else{
+            string = string + e.target.innerHTML;
+            input.value = string;
+        }
+    })
+})
